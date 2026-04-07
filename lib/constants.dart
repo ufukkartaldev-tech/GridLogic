c2907ea289b72f2c42885883ea809ed13c36611b
ufukkartaldev-tech <ufuk.kartal.dev@gmail.com>
2026-04-07 13:43:08 +0300

    feat: implement game constants and basic grid board layout

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+class GameConstants {
+  static const int rowLength = 20;
+  static const int colLength = 10;
+  static const double pixelSize = 30.0;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,14 +19,41 @@ class MyApp extends StatelessWidget {
       home: const Scaffold(
         backgroundColor: Colors.black,
         body: Center(
-          child: Text(
-            'Grid Logic',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-            ),
-          ),
+          child: GameBoard(),
         ),
+      ),
+    );
+  }
+}
+
+class GameBoard extends StatelessWidget {
+  const GameBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: GameConstants.colLength * GameConstants.pixelSize,
+      height: GameConstants.rowLength * GameConstants.pixelSize,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey[800]!, width: 2),
+      ),
+      child: GridView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: GameConstants.colLength,
+          childAspectRatio: 1.0,
+        ),
+        itemCount: GameConstants.rowLength * GameConstants.colLength,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey[700]!,
+                width: 0.5,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
