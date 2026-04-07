b9e3d889d1d21161d6c31d6cd47293eb0765381f
ufukkartaldev-tech <ufuk.kartal.dev@gmail.com>
2026-04-07 20:07:09 +0300

    feat: define geometric block shapes and neon color mappings

--- lib/values.dart
+++ lib/values.dart
@@ -31,3 +31,29 @@ class TetrominoValues {
     Tetromino.Z: [[0, 0], [1, 0], [1, 1], [2, 1]], // Z shape (3x2)
   };
 }
+
+enum BlockShape {
+  dot1x1,
+  bar2x1,
+  bar3x1,
+  square2x2,
+  Lshape3x2,
+}
+
+class BlockShapeValues {
+  static const Map<BlockShape, Color> neonColors = {
+    BlockShape.dot1x1: Color(0xFF00FFFF), // Cyan neon
+    BlockShape.bar2x1: Color(0xFFFF00FF), // Magenta neon
+    BlockShape.bar3x1: Color(0xFF00FF00), // Green neon
+    BlockShape.square2x2: Color(0xFFFFFF00), // Yellow neon
+    BlockShape.Lshape3x2: Color(0xFFFF4500), // Orange neon
+  };
+
+  static const Map<BlockShape, List<int>> coordinates = {
+    BlockShape.dot1x1: [0, 0], // Single cell (1x1)
+    BlockShape.bar2x1: [0, 0, 1, 0], // Horizontal 2-cell bar (2x1)
+    BlockShape.bar3x1: [0, 0, 1, 0, 2, 0], // Horizontal 3-cell bar (3x1)
+    BlockShape.square2x2: [0, 0, 1, 0, 0, 1, 1, 1], // 2x2 square
+    BlockShape.Lshape3x2: [0, 0, 0, 1, 1, 1, 2, 1], // L shape (3x2)
+  };
+}
